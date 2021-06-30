39d123189b9195bd0290e2c7f162e1978b719598
Guillermo Penarando <guillermo@guillerpsanchez.dev>
2021-06-30 11:43:53 +0200

    Added blake3 support Fixed monospace font

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,33 @@
 extern crate native_windows_gui as nwg;
 
 use sha2::{Sha256, Digest};
+use blake3;
 use std::env;
 use std::fs::File;
 use std::io;
 use std::rc::Rc;
+use hex;
 
-
-fn gen_hash(path: String) -> String {
+fn gen_hash_sha256(path: String) -> String {
     let mut sha256 = Sha256::new();
 
     let mut file = File::open(path).expect("Error");
 
     io::copy(&mut file, &mut sha256).expect("Error");
 
-    let hash: String = format!("{:X}", sha256.finalize());
+    let hash: String = format!("{:x}", sha256.finalize());
+
+    return hash;
+}
+
+fn gen_hash_blake3(path: String) -> std::string::String {
+    let mut blake3 = blake3::Hasher::new();
+
+    let mut file = File::open(path).expect("Error");
+
+    io::copy(&mut file, &mut blake3).expect("Error");
+
+    let hash = format!("{}", hex::encode(blake3.finalize()).to_string());
 
     return hash;
 }
@@ -24,19 +37,21 @@ fn gen_hash(path: String) -> String {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let hash = gen_hash(args[1].to_string());
-
+    let hash_sha256 = gen_hash_sha256(args[1].to_string());
+    let hash_blake3 = gen_hash_blake3(args[1].to_string());
     nwg::init().expect("Failed to init Native Windows GUI");
-    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    nwg::Font::set_global_family("Courier New").expect("Failed to set default font");
 
     let mut window = Default::default();
-    let mut text_box = Default::default();
-    let mut hash_box = Default::default();
+    let mut text_box_sha256 = Default::default();
+    let mut hash_box_sha256 = Default::default();
+    let mut text_box_blake3 = Default::default();
+    let mut hash_box_blake3 = Default::default();
 
     nwg::Window::builder()
-        .size((700, 40))
+        .size((750, 70))
         .center(true)
-        .title("SHA-256")
+        .title("Native-Hash-Calculator")
         .build(&mut window)
         .unwrap();
 
@@ -44,15 +59,32 @@ fn main() {
         .text("SHA-256:")
         .position((10,10))
         .parent(&window)
-        .build(&mut text_box)
+        .build(&mut text_box_sha256)
         .unwrap();
 
+
     nwg::TextInput::builder()
-        .text(&*hash)
-        .size((600, 20))
-        .position((85,10))
+        .text(&*hash_sha256)
+        .size((650, 20))
+        .position((95,10))
         .parent(&window)
-        .build(&mut hash_box)
+        .build(&mut hash_box_sha256)
+        .unwrap();
+
+    nwg::Label::builder()
+        .text("Blake3:")
+        .position((10,40))
+        .parent(&window)
+        .build(&mut text_box_blake3)
+        .unwrap();
+
+
+    nwg::TextInput::builder()
+        .text(&*hash_blake3)
+        .size((650, 20))
+        .position((95,40))
+        .parent(&window)
+        .build(&mut hash_box_blake3)
         .unwrap();
 
     let window = Rc::new(window);
